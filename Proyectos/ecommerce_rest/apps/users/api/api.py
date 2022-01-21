6e577aaa6d1d24174f74124355a5e90a2672b616
Santiago <santyra3030@gmail.com>
2022-01-21 11:03:58 -0500

    Modelos del proyecto

--- Proyectos/ecommerce_rest/apps/users/api/api.py
+++ Proyectos/ecommerce_rest/apps/users/api/api.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer
+from apps.users.api.serializers import UserSerializer, UserListSerializer
 from rest_framework.decorators import api_view
 
 @api_view(['GET','POST'])
@@ -13,7 +13,7 @@ def user_api_view(request):
         
         # queryset
         users = User.objects.all().values('id','username','email','password')
-        users_serializer = UserSerializer(users,many = True)
+        users_serializer = UserListSerializer(users,many = True)
         
         return Response(users_serializer.data, status=status.HTTP_200_OK)
         # test_data = {
--- Proyectos/ecommerce_rest/ecommerce_rest/settings/base.py
+++ Proyectos/ecommerce_rest/ecommerce_rest/settings/base.py
@@ -30,6 +30,8 @@ BASE_APPS = [
 
 LOCAL_APPS = [
     'apps.users',
+    'apps.products',
+    'apps.base',
 ]
 
 THIRD_APPS = [
